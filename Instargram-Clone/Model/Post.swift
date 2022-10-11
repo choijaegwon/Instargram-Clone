64a50f6193f7775e5fc0b713922b27159e8d550b
choijaegwon <chl9338@naver.com>
2022-10-11 22:01:03 +0900

    [좋아요 버튼 누르면 서버로 보내기]+[좋아요 없애면 서버에서도 삭제 가능]

--- Instargram-Clone/Model/Post.swift
+++ Instargram-Clone/Model/Post.swift
@@ -49,11 +49,15 @@ class Post {
     func adjustLikes(addLike: Bool, completion: @escaping(Int) -> ()) {
         
         guard let currentUid = Auth.auth().currentUser?.uid else { return }
-        guard let postId = postId else { return }
+        guard let postId = self.postId else { return }
         
         if addLike {
+            
             // updates user-likes structure
             USER_LIKES_REF.child(currentUid).updateChildValues([postId: 1]) { error, ref in
+                
+                // send notification to server
+                self.sendLikeNotificationToServer()
                 
                 // update post-likes structure
                 POST_LIKES_REF.child(self.postId).updateChildValues([currentUid: 1]) { error, ref in
@@ -64,21 +68,55 @@ class Post {
                 }
             }
         } else {
-            // remove like from user-like structure
-            USER_LIKES_REF.child(currentUid).child(postId).removeValue { error, ref in
+            
+            // observe database for notification id to remove
+            USER_LIKES_REF.child(currentUid).child(postId).observeSingleEvent(of: .value) { snapshot in
                 
-                // remove like from post-like structure
-                POST_LIKES_REF.child(self.postId).child(currentUid).removeValue { error, ref in
-                    guard self.likes > 0 else { return }
-                    self.likes = self.likes - 1
-                    self.didLike = false
-                    completion(self.likes)
-                    POSTS_REF.child(self.postId).child("likes").setValue(self.likes)
+                // notification id to remove from sserver
+                guard let notificationID = snapshot.value as? String else { return }
+                
+                // remove notification from server
+                NOTIFICATIONS_REF.child(self.ownerUid).child(notificationID).removeValue { err, ref in
+                    // remove like from user-like structure
+                    USER_LIKES_REF.child(currentUid).child(postId).removeValue { error, ref in
+                        
+                        // remove like from post-like structure
+                        POST_LIKES_REF.child(self.postId).child(currentUid).removeValue { error, ref in
+                            guard self.likes > 0 else { return }
+                            self.likes = self.likes - 1
+                            self.didLike = false
+                            completion(self.likes)
+                            POSTS_REF.child(self.postId).child("likes").setValue(self.likes)
+                        }
+                    }
                 }
             }
         }
+    }
+    
+    func sendLikeNotificationToServer() {
         
-
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
+        guard let postId = self.postId else { return }
         
+        let creationDate = Int(NSDate().timeIntervalSince1970)
+        
+        if currentUid != self.ownerUid {
+            
+            // notification values
+            let values = ["checked": 0,
+                          "creationDate": creationDate,
+                          "uid": currentUid,
+                          "type": LIKE_INT_VALUE,
+                          "postId": postId] as [String : Any]
+            
+            // notification database reference
+            let notificationRef = NOTIFICATIONS_REF.child(self.ownerUid).childByAutoId()
+            
+            // upload notification values to database
+            notificationRef.updateChildValues(values) { err, ref in
+                USER_LIKES_REF.child(currentUid).child(self.postId).setValue(notificationRef.key)
+            }
+        }
     }
 }
