b38e580be0fed6e533e0b9fb262d05bb962dcd0e
choijaegwon <chl9338@naver.com>
2022-10-13 05:01:31 +0900

    [채팅 컨트롤러 UI 완성하기]

--- Instargram-Clone/Controller/Messages/ChatController.swift
+++ Instargram-Clone/Controller/Messages/ChatController.swift
@@ -17,12 +17,42 @@ class ChatController: UICollectionViewController, UICollectionViewDelegateFlowLa
     var user: User?
     var messages = [Message]()
     
+    lazy var containerView: UIView = {
+        let containerView = UIView()
+        containerView.frame = CGRect(x: 0, y: 0, width: 100, height: 55)
+        
+        containerView.addSubview(messageTextField)
+        messageTextField.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: containerView.bottomAnchor, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 12, paddingBottm: 0, paddingRight: 20, width: 0, height: 0)
+        
+        let sendButton = UIButton(type: .system)
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.addTarget(self, action: #selector(handleSend), for: .touchUpInside)
+        containerView.addSubview(sendButton)
+        sendButton.anchor(top: nil, left: nil, bottom: nil, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottm: 0, paddingRight: 16, width: 0, height: 0)
+        sendButton.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        containerView.addSubview(separatorView)
+        separatorView.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: containerView.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottm: 0, paddingRight: 0, width: 0, height: 0.5)
+        
+        return containerView
+    }()
+    
+    let messageTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Enter message.."
+        return tf
+    }()
+    
     // MARK: - Init
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .white
+        
+        collectionView.register(ChatCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         
         configureNavigationBar()
     }
@@ -37,19 +67,40 @@ class ChatController: UICollectionViewController, UICollectionViewDelegateFlowLa
         tabBarController?.tabBar.isHidden = false
     }
     
+    override var inputAccessoryView: UIView? {
+        get {
+            return containerView
+        }
+    }
+    
+    override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
     // MARK: - UICollectionView
     
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width / 2, height: 50)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 5
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ChatCell
+        
+        cell.backgroundColor = .red
         
         return cell
     }
     
     // MARK: - Handlers
+    
+    @objc func handleSend() {
+        print(#function)
+        
+    }
     
     @objc func handleInfoTapped() {
         print(#function)
